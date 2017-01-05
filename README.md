56ce756fb3ad69ea11c8826dd3f3e3e708b313f3
Dmitry Ivanov <dm.vl.ivanov@gmail.com>
2017-01-05 15:26:08 +0300

    Initial release

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # PyQt5-reorderable-list-model
-Example of reorderable via drag-n-drop list model in PyQt5
+**Example of reorderable via drag-n-drop list model in PyQt5**
+
+## What's this
+
+This small script is a demo of how one can implement a model within Qt's model-view framework which items can be reordered via drag-n-drop.
+
+It is a continuation of work started in the answer to [this](http://stackoverflow.com/q/40250771/1217285) question on Stack Overflow. Now this demo uses PyQt5. It was tested with Python 3.4.3.
+
+## How it works
+
+As long as Qt's model-view framework's drag-n-drop requires the serialization of some data about the items being repositioned, the reordering via drag-n-drop can be implemented in the following way:
+
+ * On drag we serialize some info about the dragged items
+ * On drop we:
+   * deserialize this info back
+   * use this info to locate each dropped item's original position in the model - we need it to determine where to put the dropped item - before the item onto which we dropped or after it; the former option is useful if we drop onto the very first item and the latter option is useful in all other cases
+   * insert the dropped item into the appropriate position within the model
+   * if our model properly implements the `removeRows` method **and** the view's [dragDropOverwriteMode](http://doc.qt.io/qt-5/qabstractitemview.html#dragDropOverwriteMode-prop) property is set to `false` (as it is by default in `QListView` and `QTreeView` but not in `QTableView`), Qt would call it automatically after `dropMimeData` method if the latter one returns `true` thus confirming the drop was processed successfully.
+
+From the model's perspective that wraps up the whole drag-n-drop story. However, there's one more important aspect: the selection. The selection in Qt's model-view framework belongs to the view part and thus the model knows nothing about the selection and how to update it properly. So we need to tinker with the view part as well if we want it to behave nicely.
+
+Perhaps the simplest and most intuitive behaviour would be to have the dropped items selected and one of them to be set as the current item (`selected` and `current` are two different concepts within [QItemSelectionModel](http://doc.qt.io/qt-5/qitemselectionmodel.html)) after the reordering is finished. I haven't found a way to achieve this with standard `QItemSelectionModel` so I implemented a custom subclass of it and added a couple of things to the model to provide some useful info to the selection model:
+ * The model now stores the list of last dropped items updated near the end of `dropMimeData` method
+ * The model also sets the boolean flag in the end of `dropMimeData` method thus indicating that the next call to `removeRows` should be treated as the call performed by Qt automatically which indicates the end of drag-n-drop events sequence. So in the end of `removeRows` the model checks this flag and if it's set, it drops the flag back and emits a custom signal `dragDropFinished` which intends to informs whoever is listening about the fact that the whole drag-n-drop sequence is finished from the model's perspective.
+ * The selection model connects a special slot to this model's signal in which it fetches the list of last dropped items from the model, figures out which rows these items now correspond to (using another custom method within the model - `rowForItem`) and selects these rows. It also sets the last of these rows as the current one.
---
+++ reorderable_list_model.py
@@ -0,0 +1,203 @@
+import sys
+from PyQt5 import QtWidgets, QtCore
+
+class ReorderableListModel(QtCore.QAbstractListModel):
+    '''
+    ReorderableListModel is a list model which implements reordering of its
+    items via drag-n-drop
+    '''
+    dragDropFinished = QtCore.pyqtSignal()
+
+    def __init__(self, parent=None):
+        QtCore.QAbstractItemModel.__init__(self, parent)
+        self.nodes = ['node0', 'node1', 'node2', 'node3', 'node4', 'node5']
+        self.lastDroppedItems = []
+        self.pendingRemoveRowsAfterDrop = False
+
+    def rowForItem(self, text):
+        '''
+        rowForItem method returns the row corresponding to the passed in item
+        or None if no such item exists in the model
+        '''
+        try:
+            row = self.nodes.index(text)
+        except ValueError:
+            return None
+        return row
+
+    def index(self, row, column, parent):
+        if row < 0 or row >= len(self.nodes):
+            return QtCore.QModelIndex()
+        return self.createIndex(row, column)
+
+    def parent(self, index):
+        return QtCore.QModelIndex()
+
+    def rowCount(self, index):
+        if index.isValid():
+            return 0
+        return len(self.nodes)
+
+    def data(self, index, role):
+        if not index.isValid():
+            return None
+        if role == QtCore.Qt.DisplayRole:
+            row = index.row()
+            if row < 0 or row >= len(self.nodes):
+                return None
+            return self.nodes[row]
+        else:
+            return None
+
+    def supportedDropActions(self):
+        return QtCore.Qt.MoveAction
+
+    def flags(self, index):
+        if not index.isValid():
+            return QtCore.Qt.ItemIsEnabled
+        return QtCore.Qt.ItemIsEnabled | QtCore.Qt.ItemIsSelectable | \
+               QtCore.Qt.ItemIsDragEnabled | QtCore.Qt.ItemIsDropEnabled
+
+    def insertRows(self, row, count, index):
+        if index.isValid():
+            return False
+        if count <= 0:
+            return False
+        # inserting 'count' empty rows starting at 'row'
+        self.beginInsertRows(QtCore.QModelIndex(), row, row + count - 1)
+        for i in range(0, count):
+            self.nodes.insert(row + i, '')
+        self.endInsertRows()
+        return True
+
+    def removeRows(self, row, count, index):
+        if index.isValid():
+            return False
+        if count <= 0:
+            return False
+        num_rows = self.rowCount(QtCore.QModelIndex())
+        self.beginRemoveRows(QtCore.QModelIndex(), row, row + count - 1)
+        for i in range(count, 0, -1):
+            self.nodes.pop(row - i + 1)
+        self.endRemoveRows()
+
+        if self.pendingRemoveRowsAfterDrop:
+            '''
+            If we got here, it means this call to removeRows is the automatic
+            'cleanup' action after drag-n-drop performed by Qt
+            '''
+            self.pendingRemoveRowsAfterDrop = False
+            self.dragDropFinished.emit()
+
+        return True
+
+    def setData(self, index, value, role):
+        if not index.isValid():
+            return False
+        if index.row() < 0 or index.row() > len(self.nodes):
+            return False
+        self.nodes[index.row()] = str(value)
+        self.dataChanged.emit(index, index)
+        return True
+
+    def mimeTypes(self):
+        return ['application/vnd.treeviewdragdrop.list']
+
+    def mimeData(self, indexes):
+        mimedata = QtCore.QMimeData()
+        encoded_data = QtCore.QByteArray()
+        stream = QtCore.QDataStream(encoded_data, QtCore.QIODevice.WriteOnly)
+        for index in indexes:
+            if index.isValid():
+                text = self.data(index, 0)
+        stream << QtCore.QByteArray(text.encode('utf-8'))
+        mimedata.setData('application/vnd.treeviewdragdrop.list', encoded_data)
+        return mimedata
+
+    def dropMimeData(self, data, action, row, column, parent):
+        if action == QtCore.Qt.IgnoreAction:
+            return True
+        if not data.hasFormat('application/vnd.treeviewdragdrop.list'):
+            return False
+        if column > 0:
+            return False
+
+        num_rows = self.rowCount(QtCore.QModelIndex())
+        if num_rows <= 0:
+            return False
+
+        if row < 0:
+            if parent.isValid():
+                row = parent.row()
+            else:
+                return False
+
+        encoded_data = data.data('application/vnd.treeviewdragdrop.list')
+        stream = QtCore.QDataStream(encoded_data, QtCore.QIODevice.ReadOnly)
+
+        new_items = []
+        rows = 0
+        while not stream.atEnd():
+            text = QtCore.QByteArray()
+            stream >> text
+            text = bytes(text).decode('utf-8')
+            index = self.nodes.index(text)
+            new_items.append((text, index))
+            rows += 1
+
+        self.lastDroppedItems = []
+        for (text, index) in new_items:
+            target_row = row
+            if index < row:
+                target_row += 1
+            self.beginInsertRows(QtCore.QModelIndex(), target_row, target_row)
+            self.nodes.insert(target_row, text)
+            self.endInsertRows()
+            self.lastDroppedItems.append(text)
+            row += 1
+
+        self.pendingRemoveRowsAfterDrop = True
+        return True
+
+class SelectionModel(QtCore.QItemSelectionModel):
+    def __init__(self, parent=None):
+        QtCore.QItemSelectionModel.__init__(self, parent)
+
+    def onModelItemsReordered(self):
+        new_selection = QtCore.QItemSelection()
+        new_index = QtCore.QModelIndex()
+        for item in self.model().lastDroppedItems:
+            row = self.model().rowForItem(item)
+            if row is None:
+                continue
+            new_index = self.model().index(row, 0, QtCore.QModelIndex())
+            new_selection.select(new_index, new_index)
+
+        self.clearSelection()
+        flags = QtCore.QItemSelectionModel.ClearAndSelect | \
+                QtCore.QItemSelectionModel.Rows | \
+                QtCore.QItemSelectionModel.Current
+        self.select(new_selection, flags)
+        self.setCurrentIndex(new_index, flags)
+
+class MainForm(QtWidgets.QMainWindow):
+    def __init__(self, parent=None):
+        QtWidgets.QMainWindow.__init__(self, parent)
+        self.model = ReorderableListModel()
+        self.selectionModel = SelectionModel(self.model)
+        self.model.dragDropFinished.connect(self.selectionModel.onModelItemsReordered)
+        self.view = QtWidgets.QListView()
+        self.view.setModel(self.model)
+        self.view.setSelectionModel(self.selectionModel)
+        self.view.setDragDropMode(QtWidgets.QAbstractItemView.InternalMove)
+        self.view.setDragDropOverwriteMode(False)
+        self.setCentralWidget(self.view)
+
+def main():
+    app = QtWidgets.QApplication(sys.argv)
+    form = MainForm()
+    form.show()
+    app.exec_()
+
+if __name__ == '__main__':
+    main()
